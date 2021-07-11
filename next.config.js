1ff379e76026bb414e0feeb4d7658add52a7d0c2
Nick Zou <z.nick00@gmail.com>
2021-07-10 23:54:44 -0400

    refactor layout to add global layout to _app.tsx

--- next.config.js
+++ next.config.js
@@ -8,5 +8,5 @@ module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
-  }
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from '../components/Head';
+
 import Header from '../components/Header/Header';
 import Hero from './index/Hero';
 
 export default function Home() {
   return (
-    <>
-      <Head />
-      <body>
-        <Header />
-        <Hero />
-      </body>
-    </>
+    <main>
+      <Hero />
+    </main>
   )
 }
