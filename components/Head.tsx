eb0259a32c2e07eeb61b8ef9b2bd4ca83db5114b
Nick Zou <z.nick00@gmail.com>
2021-07-10 20:11:52 -0400

    add custom Head component

---
+++ components/Head.tsx
@@ -0,0 +1,10 @@
+import NextHead from 'next/head';
+
+export default function Head() {
+    return(
+        <NextHead>
+            <title>Nick Zou | Designer, Developer, Manager</title>
+            <meta name="description" content="Toronto based designer, front-end developer, and web development manager. Here to help you solve your technical challenges and bring your product to market quickly and efficiently."></meta>
+        </NextHead>
+    )
+};
