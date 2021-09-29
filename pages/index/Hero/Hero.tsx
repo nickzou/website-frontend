77bac39316aa5bb1072e883fdcdd4d555702ca19
Nick Zou <z.nick00@gmail.com>
2021-09-28 22:12:00 -0400

    add bootstrap classes for desktop

--- pages/index/Hero/Hero.tsx
+++ pages/index/Hero/Hero.tsx
@@ -7,20 +7,20 @@ export default function Hero() {
         <section id="hero" className={`position-relative ${styles.hero}`}>
             <div className={`container-md white ${styles.content}`}>
                 <div className={`row justify-content-center justify-content-lg-start ${styles.row__content_landscape}`}>
-                    <div className={`col col-11 offset-lg-1 ${styles.col__content_landscape}`}>
+                    <div className={`col col-11 offset-lg-1 offset-xl-0 ${styles.col__content_landscape}`}>
                         <hgroup className={styles.hgroup}>
                             <h1 className={styles.site_title}><span className={styles.outline}>nick</span> zou</h1>
-                            <div className={`mb-3 ${styles.site_subtitle}`}>designer. developer. manager. <br/>based out of Toronto, Canada.</div>
+                            <div className={`mb-3 ${styles.site_subtitle}`}>designer. developer. manager.</div>
                         </hgroup>
                     </div>
                 </div>
                 <div className={`row mb-3 justify-content-center justify-content-lg-start ${styles.row__content_landscape}`}>
-                    <div className={`col col-9 col-md-4 col-lg-3 offset-lg-1 ${styles.col__cta} ${styles.col__cta_landscape}`}>
+                    <div className={`col col-9 col-md-4 col-lg-3 offset-lg-1 offset-xl-0 ${styles.col__cta} ${styles.col__cta_landscape}`}>
                         <Button link={'#portfolio'} text={'see my work'} />
                     </div>
                 </div>
                 <div className={`row mb-3 justify-content-center justify-content-lg-start ${styles.row__content_landscape}`}>
-                    <div className={`col col-9 col-md-4 col-lg-3 offset-lg-1 ${styles.col__cta} ${styles.col__cta_landscape}`}>
+                    <div className={`col col-9 col-md-4 col-lg-3 offset-lg-1 offset-xl-0 ${styles.col__cta} ${styles.col__cta_landscape}`}>
                         <Button link={'#contact'} text={'connect with me'} outline={true}/>
                     </div>
                 </div>
@@ -30,5 +30,5 @@ export default function Hero() {
                 overlay={true}
             />
         </section>
-    );
-};
+    )
+}
