121233b57c8e009803af2bae8a045edf37bd3936
Nick Zou <z.nick00@gmail.com>
2021-09-28 22:30:04 -0400

    add section-title class

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Hero from './index/Hero/Hero';
+import About from './index/About/About';
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   )
 }
--- pages/index/About/About.tsx
+++ pages/index/About/About.tsx
@@ -1,7 +1,14 @@
 export default function About() {
     return(
         <section id="about" className={`position-relative`}>
-
+            <div className="container">
+                <div className="row">
+                    <div className="col col-9 offset-2">
+                        <h2 className="section-title">jack of all trades, master of some</h2>
+                        <p>this is the text lorem ipsum this is the text lorem ipsum this is the text lorem ipsum this is the text lorem ipsum this is the text lorem ipsum this is the text lorem ipsum</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
